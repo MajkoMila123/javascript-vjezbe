63c366369514406e5c101d5483798b5bcabd4251
MajkoMila123 <114142012+MajkoMila123@users.noreply.github.com>
2022-09-22 10:17:15 +0200

    Add files via upload

---
+++ vjezba2/script.js
@@ -0,0 +1,9 @@
+function pokaziDatum() {
+    datum = document.getElementById("datum");
+    datum.innerHTML = Date();
+}
+
+function pokaziGodinu() {
+    godina = document.getElementById("godina");
+    godina.innerHTML = new Date().getFullYear();
+}
